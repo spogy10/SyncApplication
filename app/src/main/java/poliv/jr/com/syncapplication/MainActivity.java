7ee3d3423e8699dcc7bd7b17cb025317d78e3edb
poliverjr <poliverjr@hotmail.com>
2019-07-28 02:51:29 -0500

    created Utility class for globally used functions added read/write permission to manifest

--- app/src/main/java/poliv/jr/com/syncapplication/MainActivity.java
+++ app/src/main/java/poliv/jr/com/syncapplication/MainActivity.java
@@ -3,12 +3,19 @@ package poliv.jr.com.syncapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+import poliv.jr.com.syncapplication.utility.Utility;
+
+public class MainActivity extends AppCompatActivity { //todo: insert request permissions
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Utility.outputVerbose(new File(Utility.getFolderPath()).listFiles()[0].getName());
     }
 }
---
+++ app/src/main/java/poliv/jr/com/syncapplication/utility/Utility.java
@@ -0,0 +1,43 @@
+package poliv.jr.com.syncapplication.utility;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+
+public class Utility {
+
+    private static final String DEFAULT_ITEM_FOLDER_NAME = "Videos";
+
+    private static final String LOG_TAG = "Verbose";
+
+    /* Checks if external storage is available for read and write */
+    public static boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    /* Checks if external storage is available to at least read */
+    public static boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
+    }
+
+
+    public static String getFolderPath(){
+        return Environment.getExternalStoragePublicDirectory("") + File.separator + DEFAULT_ITEM_FOLDER_NAME;
+    }
+
+    public static void outputVerbose(String message){
+        Log.d(LOG_TAG, message);
+    }
+
+    public static void outputError(Exception e){
+        outputError("", e);
+    }
+
+    public static void outputError(String message, Exception e){
+        Log.e(LOG_TAG, message, e);
+    }
+}
