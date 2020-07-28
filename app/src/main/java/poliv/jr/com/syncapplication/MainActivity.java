4d9d1091953b8d0c01f093f3350078c81e3806ca
poliverjr <poliverjr@hotmail.com>
2020-07-28 00:01:06 -0500

    remove worker handler implementation

--- app/src/main/java/poliv/jr/com/syncapplication/MainActivity.java
+++ app/src/main/java/poliv/jr/com/syncapplication/MainActivity.java
@@ -4,10 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.work.OneTimeWorkRequest;
-import androidx.work.Operation;
-import androidx.work.WorkManager;
-import androidx.work.WorkRequest;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
@@ -16,18 +12,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-
-import library.sharedpackage.models.FileContent;
 import poliv.jr.com.syncapplication.manager.ClientRemoteItemManager;
 import poliv.jr.com.syncapplication.manager.FileManager;
-import poliv.jr.com.syncapplication.server.Server;
 import poliv.jr.com.syncapplication.utility.Utility;
-import poliv.jr.com.syncapplication.worker.FileTransferWorker;
 
 //todo: create notifications
-//todo: for background services that probably won't close connection on sleep https://developer.android.com/guide/components/services
+//todo: for foreground services that probably won't close connection on sleep https://developer.android.com/guide/components/services
+//todo: run server connection in foreground service
 
 
 public class MainActivity extends AppCompatActivity {
@@ -80,22 +71,6 @@ public class MainActivity extends AppCompatActivity {
     private void stopServer(){
         if(fileManager != null)
             fileManager.stopServer();
-    }
-
-    public boolean transferFilesInWorker(List<FileContent> files, boolean send, Server sever){
-        WorkRequest transferFileRequest =
-                new OneTimeWorkRequest.Builder(FileTransferWorker.class)
-                        .build();
-
-        Operation op = WorkManager.getInstance(this).enqueue(transferFileRequest);
-        try {
-            return op.getResult().get() == Operation.SUCCESS;
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return true;
     }
 
     private void checkForWritePermission(){
