bdf1d2559cfac3db3f414f6a18ae0ccb82fc1081
poliverjr <poliverjr@hotmail.com>
2020-07-28 23:20:29 -0500

    set up notification channel in Application class

--- app/src/main/java/poliv/jr/com/syncapplication/SyncApplication.java
+++ app/src/main/java/poliv/jr/com/syncapplication/SyncApplication.java
@@ -1,4 +1,26 @@
 package poliv.jr.com.syncapplication;
 
-public class SyncApplication {
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+
+public class SyncApplication extends Application {
+    public static final String NOTIFICATION_CHANNEL_ID = "syncappServiceChannel";
+    private static final String NOTIFICATION_CHANNEL_NAME = "Sync App Notification";
+    private static final int NOTIFICATION_IMPORTANCE_LEVEL = NotificationManager.IMPORTANCE_LOW;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        NotificationChannel serviceChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_NAME,
+                NOTIFICATION_IMPORTANCE_LEVEL);
+
+        NotificationManager manager = getSystemService(NotificationManager.class);
+        manager.createNotificationChannel(serviceChannel);
+    }
 }
